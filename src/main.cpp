7b96256b9d7a53cf6c1ed136d143998075af54ea
tomikaa87 <tomikaa87@gmail.com>
2020-06-02 17:55:03 +0200

    Add code for interrupt latency measurement

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,18 @@
 #include <ESP8266WebServer.h>
 #include <FS.h>
 
+//
+// Pin falling edge interrupt latency: 5us - 18us (5.5us - 6.3us avg)
+// FIXME: rising edge interrupt is buggy, IRS called continuously while the input is high
+//
+
 static ESP8266WebServer webServer(80);
+
+ICACHE_RAM_ATTR void isr()
+{
+    digitalWrite(D7, HIGH);
+    digitalWrite(D7, LOW);
+}
 
 void setup()
 {
@@ -24,6 +35,12 @@ void setup()
     webServer.begin();
 
     Serial.println("Initialization finished");
+
+    pinMode(D7, OUTPUT);
+    digitalWrite(D7, LOW);
+
+    pinMode(D8, INPUT);
+    attachInterrupt(D8, isr, FALLING);
 }
 
 void loop()
