d13b1bc985cddccf4a5095aa06cfb4503b874f13
tomikaa87 <tomikaa87@gmail.com>
2021-08-18 14:09:54 +0200

    Update esp-iot-base, fix warnings, pin lib deps

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ void setup()
 {
     static ApplicationConfig appConfig;
 
-    appConfig.firmwareVersion = VersionNumber{ 1, 5, 1 };
+    appConfig.firmwareVersion = VersionNumber{ 1, 5, 2 };
 
     appConfig.blynk.appToken = Config::Blynk::AppToken;
     appConfig.blynk.serverHostName = Config::Blynk::ServerHostName;
